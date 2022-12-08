762ddb2f960f951af07b46e3d36feeae6bcfdd6c
yuunull <yuunull2525@gmail.com>
2022-12-08 21:49:57 +0900

    update

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>テスト</Text>
+      <Button
+          title="Click Me"
+          onPress={()=>{
+            alert("テスト");
+            console.log("コンソールログ")
+          }}
+        />
       <StatusBar style="auto" />
     </View>
   );
